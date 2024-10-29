61f348160cb7fa6760bc5e4bfbcca5db377b404d
Beebajanu <101020548+Beebajanu@users.noreply.github.com>
2024-10-29 21:22:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "3jo1QYII#J0Bt_vn4z0m8pT21MPNltD6hbIL3Gx3_qK9rvgHWbTY",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
